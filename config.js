1e74c319eab928bbc523c6cce083ced9449c685c
Andrew Gunn <andrew.gunn@oakbrookfinance.com>
2014-01-21 13:44:48 +0000

    Added Ghost blogging platform

---
+++ config.js
@@ -0,0 +1,45 @@
+// # Ghost Configuration
+// Setup your Ghost install for various environments
+
+var path = require('path'),
+    config;
+
+config = {
+    // ### Development **(default)**
+    development: {
+        url: 'http://127.0.0.1:1234',
+        database: {
+            client: 'sqlite3',
+            connection: {
+                filename: path.join(__dirname, '/content/data/ghost-dev.db')
+            },
+            debug: false
+        },
+        server: {
+            host: '127.0.0.1',
+            port: '1234'
+        }
+    },
+
+    // ### Production
+    // When running Ghost in the wild, use the production environment
+    // Configure your URL and mail settings here
+    production: {
+        url: 'http://sequensis.azurewebsites.net',
+        mail: {},
+        database: {
+            client: 'sqlite3',
+            connection: {
+                filename: path.join(__dirname, '/content/data/ghost.db')
+            },
+            debug: false
+        },
+        server: {
+            host: '127.0.0.1',
+            port: process.env.PORT
+        }
+    }
+};
+
+// Export config
+module.exports = config;
