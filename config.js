938253dfce46149ad5856948d1e505b644726dff
Andrew Gunn <andrew.gunn@sequensis..co.uk>
2014-03-17 11:57:03 +0000

    Updated the URL (blog.sequensis.co.uk).

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://sequensisblog.azurewebsites.net',
+        url: 'http://blog.sequensis.co.uk',
         mail: {
             transport: 'SMTP',
             options: {
