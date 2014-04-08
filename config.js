79e0b43079838b340288eba1fbe6542952dd4087
Andrew Gunn <andrew.gunn@sequensis..co.uk>
2014-04-08 16:49:47 +0100

    Fixed random Windows Azure error.

--- config.js
+++ config.js
@@ -7,7 +7,24 @@ var path = require('path'),
 config = {
     // ### Development **(default)**
     development: {
-        url: 'http://127.0.0.1:1234',
+        // The url to use when providing links to the site, E.g. in RSS and email.
+        url: 'http://sequensis.azurewebsites.net',
+
+        // Example mail config
+        // Visit http://docs.ghost.org/mail for instructions
+
+        mail: {
+            transport: 'SMTP',
+            options: {
+                service: 'Gmail',
+                auth: {
+                    user: 'postmaster@blog.sequensis.co.uk', // mailgun username
+                    pass: '6pe50vcgx847'  // mailgun password
+                }
+            }
+        },
+
+
         database: {
             client: 'sqlite3',
             connection: {
@@ -16,7 +33,9 @@ config = {
             debug: false
         },
         server: {
+            // Host to be passed to node's `net.Server#listen()`
             host: '127.0.0.1',
+            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
             port: '1234'
         }
     },
@@ -29,10 +48,10 @@ config = {
         mail: {
             transport: 'SMTP',
             options: {
-                service: 'Mailgun',
+                service: 'Gmail',
                 auth: {
-                    user: 'postmaster@blog.sequensis.co.uk',
-                    pass: '6pe50vcgx847'
+                    user: 'postmaster@blog.sequensis.co.uk', // mailgun username
+                    pass: '6pe50vcgx847'  // mailgun password
                 }
             }
         },
@@ -44,8 +63,65 @@ config = {
             debug: false
         },
         server: {
+            // Host to be passed to node's `net.Server#listen()`
             host: '127.0.0.1',
+            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
             port: process.env.PORT
+        }
+    },
+
+    // **Developers only need to edit below here**
+
+    // ### Testing
+    // Used when developing Ghost to run tests and check the health of Ghost
+    // Uses a different port number
+    testing: {
+        url: 'http://127.0.0.1:2369',
+        database: {
+            client: 'sqlite3',
+            connection: {
+                filename: path.join(__dirname, '/content/data/ghost-test.db')
+            }
+        },
+        server: {
+            host: '127.0.0.1',
+            port: '2369'
+        }
+    },
+
+    // ### Travis
+    // Automated testing run through GitHub
+    'travis-sqlite3': {
+        url: 'http://127.0.0.1:2369',
+        database: {
+            client: 'sqlite3',
+            connection: {
+                filename: path.join(__dirname, '/content/data/ghost-travis.db')
+            }
+        },
+        server: {
+            host: '127.0.0.1',
+            port: '2369'
+        }
+    },
+
+    // ### Travis
+    // Automated testing run through GitHub
+    'travis-mysql': {
+        url: 'http://127.0.0.1:2369',
+        database: {
+            client: 'mysql',
+            connection: {
+                host     : '127.0.0.1',
+                user     : 'travis',
+                password : '',
+                database : 'ghost_travis',
+                charset  : 'utf8'
+            }
+        },
+        server: {
+            host: '127.0.0.1',
+            port: '2369'
         }
     }
 };
